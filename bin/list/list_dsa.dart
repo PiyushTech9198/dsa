355a3ec6886af7227943157e85eb10fce95fec8a
PiyushLovoj <105102970+PiyushFx@users.noreply.github.com>
2024-01-12 10:30:14 +0530

    List

--- bin/list/list_dsa.dart
+++ bin/list/list_dsa.dart
@@ -1,5 +1,6 @@
-class ListDsa {
+import 'dart:math';
 
+class ListDsa {
   //// Largest in List \\\\
 
   static double largestNumberInList({required List<double> listOfNumbers}) {
@@ -25,4 +26,72 @@ class ListDsa {
     }
     return smallestNumber;
   }
+
+  // Binary Search on List
+int binarySearch({required List<int> listOfNumber, required int value}) {
+  int start = 0;
+  int end = listOfNumber.length - 1;
+
+  while (start <= end) {
+    int mid = (start + end) ~/ 2;
+
+    // Comparison
+    if (listOfNumber[mid] == value) {
+      return mid;
+    }
+
+    if (listOfNumber[mid] < value) {
+      start = mid + 1; // Fix: Move the 'start' index to the next element
+    } else {
+      end = mid - 1; // Fix: Move the 'end' index to the previous element
+    }
+  }
+
+  return -1; // Return -1 if the value is not found
 }
+
+
+
+
+
+   // Using for loop \\
+
+  static void reverseForLoop({required List<int> numbers}) {
+
+  // Manually swap elements
+  for (int i = 0, j = numbers.length - 1; i < j; i++, j--) {
+    int temp = numbers[i];
+    numbers[i] = numbers[j];
+    numbers[j] = temp;
+  }
+
+}
+
+
+///  Using while loop \\
+
+static void reverseWhileLoop({required List<int> numbers}){
+
+  // Using while loop to reverse the array in-place
+  int start = 0;
+  int end = numbers.length - 1;
+
+  while (start < end) {
+    // Swap elements at start and end indices
+    int temp = numbers[start];
+    numbers[start] = numbers[end];
+    numbers[end] = temp;
+
+    // Move indices towards the center
+    start++;
+    end--;
+  }
+
+  }
+
+}
+
+
+
+
+
--- bin/main.dart
+++ bin/main.dart
@@ -1,7 +1,15 @@
 import 'list/list_dsa.dart';
 
-void main(List<String> arguments) {
-  print(ListDsa.largestNumberInList(listOfNumbers: [1, 2, 4, 3231, 3, 12, 31, -10,-100,09,0]));
-  print(
-      ListDsa.smallestNumberInList(listOfNumbers: [1, 2, 4, 3231, 3, 12, 31,-1,-100,0]));
+void main() {
+  List<int> numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  int searchValue = 7;
+
+  int result =
+      ListDsa().binarySearch(listOfNumber: numbers, value: searchValue);
+
+  if (result != -1) {
+    print('$searchValue found at index $result');
+  } else {
+    print('$searchValue not found in the list');
+  }
 }
